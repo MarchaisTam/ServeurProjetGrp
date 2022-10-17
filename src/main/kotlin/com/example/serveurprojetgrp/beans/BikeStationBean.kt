5768cca91f7b5b546b8aad873d7b4834bdfb0d48
flo <fmaury.dev@free.fr>
2022-10-17 10:33:14 +0200

    refactored beans-entities

--- src/main/kotlin/com/example/serveurprojetgrp/beans/BikeStationBean.kt
+++ src/main/kotlin/com/example/serveurprojetgrp/beans/BikeStationBean.kt
@@ -1,46 +1,51 @@
 package com.example.serveurprojetgrp.beans
 
 import com.google.gson.annotations.SerializedName
+import org.hibernate.Hibernate
+import javax.persistence.*
 
-data class BikeStationBean(
+@Entity
+@Table(name = "bike_station")
+data class BikeStationEntity(
 
-    var name: String,
-    var address: String,
-    var position: CoordBean?,
-    var banking: Boolean?,
+    var name: String = "",
+    var address: String = "",
+
+    @Embedded
+    var position: CoordBean? = null,
+
+    var banking: Boolean? = null,
 
     @SerializedName("bike_stands")
-    var stands: Int?,
+    var stands: Int? = null,
     @SerializedName("available_bike_stands")
-    var availableStands: Int?,
+    var availableStands: Int? = null,
     @SerializedName("available_bikes")
-    var availableBikes: Int?,
+    var availableBikes: Int? = null,
 
-    var status: String,
+    var status: String = "",
 
     @SerializedName("last_update")
-    var lastUpdate: Long?
+    var lastUpdate: Long? = null,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
 
 ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as BikeStationEntity
 
-    fun hasAvailableStands() = availableStands!! > 0
+        return id != null && id == other.id
+    }
 
-    fun hasAvailableBikes() = availableBikes!! > 0
+    override fun hashCode(): Int = javaClass.hashCode()
 
-    fun toEntity() = BikeStationEntity(
-        name,
-        address,
-        position,
-        banking,
-        stands,
-        availableStands,
-        availableBikes,
-        status,
-        lastUpdate
-    )
-
+    @Override
     override fun toString(): String {
-        return "name='$name', address='$address', banking=$banking, availableStands=$availableStands, availableBikes=$availableBikes, status='$status'"
+        return this::class.simpleName + "(name = $name , address = $address , position = $position, banking = $banking , availableStands = $availableStands , availableBikes = $availableBikes , status = $status )"
     }
 
 }
